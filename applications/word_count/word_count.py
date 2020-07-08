ed5a428166d0975ca6f8f1dd09c40b4951afeebf
janecyyu <janecyyu@gmail.com>
2020-07-08 15:04:24 -0700

    count word ok

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,24 @@
-def word_count(s):
-    # Your code here
+import string
 
+
+def word_count(s):
+    for c in string.punctuation:
+        if c is not "'":
+            s = s.replace(c, "")
+    counts = {}
+    s = s.lower()
+    s = s.split()
+    for word in s:
+        if word not in counts:
+            counts[word] = 0
+        counts[word] += 1
+    print("counts", counts)
+    return counts
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
