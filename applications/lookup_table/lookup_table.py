7d1d3de645fe7691e098bc21da44681e95c7a3cb
janecyyu <janecyyu@gmail.com>
2020-07-08 13:50:49 -0700

    lookup faster

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,6 +1,7 @@
 # Your code here
 import random
 import math
+cache = {}
 
 
 def slowfun_too_slow(x, y):
@@ -18,14 +19,17 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-    cache = {}
+
     key = math.pow(x, y)
-    if key not in cache:
+    if key not in cache:  # if key's not a key in the cache dict
         cache[key] = slowfun_too_slow(x, y)
+    print(cache)
+    print(cache[key])
     return cache[key]
 
 
 # Do not modify below this line!
+
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
