3fb6a99e27041c4e4ced5e68eb8e8ada688258aa
janecyyu <janecyyu@gmail.com>
2020-07-09 13:23:46 -0700

    markov can print

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -8,16 +8,30 @@ with open("input.txt") as f:
 # TODO: analyze which words can follow other words
 # Your code here
 # put every words to key
+# test = ["Cats", "and", "dogs", "and", "birds"]
 myDict = {}
-length = words.len()
+length = len(words)
 for i in range(length-1):
     w = words[i]
     if w not in myDict:
-        arr = []
-        myDict[w] = arr.append(words[i+1])
+        myDict[w] = []
     # if exists in myDict, add it to the value(array type):
     myDict[w].append(words[i+1])
 
 
 # TODO: construct 5 random sentences
 # Your code here
+# random start
+start_word = random.choice(words)
+stop_word = random.choice(words)
+print("start:", start_word)
+print("end:", stop_word)
+for k, v in myDict.items():
+    if k == stop_word:
+        break
+    print(k, end=" ")
+    # random value
+    if len(v) == 1:
+        print(v[0], end=" ")
+    else:
+        print(random.choice(v), end=" ")
