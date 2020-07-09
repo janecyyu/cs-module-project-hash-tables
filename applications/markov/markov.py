1a7ac5c1d6e6a00c9ad8f69557d159339da96685
janecyyu <janecyyu@gmail.com>
2020-07-09 14:04:55 -0700

    start stop word ok

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -22,16 +22,44 @@ for i in range(length-1):
 # TODO: construct 5 random sentences
 # Your code here
 # random start
-start_word = random.choice(words)
-stop_word = random.choice(words)
+start_words = []
+for k in myDict:
+    condition = k[0] == '"' and k[1].isupper()
+    if k[0].isupper() or condition:
+        start_words.append(k)
+# start_words = " ".join(start_words)
+
+stop_words = []
+for k in myDict:
+    conditionA = k[len(k)-2] == "." and k[len(k)-1] == '"'
+    conditionB = k[len(k)-2] == "?" and k[len(k)-1] == '"'
+    conditionC = k[len(k)-2] == "!" and k[len(k)-1] == '"'
+    if conditionA or conditionB or conditionC or k[len(k)-1] == "!" or k[len(k)-1] == "?" or k[len(k)-1] == ".":
+        stop_words.append(k)
+
+start_word = random.choice(start_words)
+stop_word = random.choice(stop_words)
 print("start:", start_word)
 print("end:", stop_word)
-for k, v in myDict.items():
-    if k == stop_word:
-        break
-    print(k, end=" ")
-    # random value
-    if len(v) == 1:
-        print(v[0], end=" ")
-    else:
-        print(random.choice(v), end=" ")
+
+
+# for k, v in myDict.items():
+#     if k == stop_word:
+#         break
+#     print(k, end=" ")
+#     # random value
+#     if len(v) == 1:
+#         print(v[0], end=" ")
+#     else:
+#         print(random.choice(v), end=" ")
+
+
+# for k, v in myDict.items():
+#     if k == stop_word:
+#         break
+#     print(k, end=" ")
+#     # random value
+#     if len(v) == 1:
+#         print(v[0], end=" ")
+#     else:
+#         print(random.choice(v), end=" ")
