ff389f6669a38efa3f033e29c8f69c29e7d35189
janecyyu <janecyyu@gmail.com>
2020-07-09 15:08:57 -0700

    histo ok

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,26 @@
 # Your code here
+import string
 
+with open("robin.txt") as f:
+    words = f.read()
+
+# words = "dog, dog, dog, hot, hot, cat, car"
+
+# # remove punctuation
+for c in string.punctuation:
+    # except '
+    if c != "'":
+        words = words.replace(c, "")
+
+# split it into words.
+words = words.split()
+counts = {}
+
+for w in words:
+    if w not in counts:
+        counts[w] = []
+    counts[w].append("#")
+
+for k, v in counts.items():
+    print(k, end=" ")
+    print("".join(v))
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,8 +5,7 @@ with open("input.txt") as f:
     words = f.read()
     # split it into words.
     words = words.split()
-# TODO: analyze which words can follow other words
-# Your code here
+
 # put every words to key
 # test = ["Cats", "and", "dogs", "and", "birds"]
 myDict = {}
@@ -18,8 +17,6 @@ for i in range(length-1):
     # if exists in myDict, add it to the value(array type):
     myDict[w].append(words[i+1])
 
-
-# TODO: construct 5 random sentences
 # Your code here
 
 # get start words
